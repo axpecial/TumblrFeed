96dcdfec8c157b4cf2243cd7fdffdb5a394cb621
axpecial <and043@ucsd.edu>
2018-01-31 22:29:15 -0800

    Created a README.md.

---
+++ TumblrFeed/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  TumblrFeed
+//
+//  Created by Andy Duong on 1/31/18.
+//  Copyright © 2018 Andy Duong. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
